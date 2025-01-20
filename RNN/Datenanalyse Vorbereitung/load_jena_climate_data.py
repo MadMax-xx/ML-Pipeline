4c0e534592c2fdf20eb1f1a7c54d4d983d85dc3c
Wafaa <arzanewafae049@gmail.com>
2025-01-20 21:23:35 +0100

    test

--- RNN/Datenanalyse Vorbereitung/load_jena_climate_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-file_path = "C:/Users/bilal/OneDrive - Hochschule Düsseldorf/Desktop/HSD/ML/Projektsarbeit/ml-ws2425-team6-2/RNN/Datenanalyse Vorbereitung/jena_climate_2014_2016.csv"
-data = pd.read_csv(file_path, parse_dates=["Date Time"], index_col="Date Time", encoding="utf-8")   #ich hatte Probleme bei Laden der Datei, hab aber eine Lösung mit Notepad++ Encding gefunden durch UTF-()
-
-print("Datei erfolgreich geladen!")
-print(data.head())
-print(data.info())
